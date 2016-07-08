c94eaa964fd9ae3d79ef459992f0afe8bb875cb4
JPK <japakaiser@gmail.com>
2016-07-09 01:05:06 +0200

    Added multiteam support

---
+++ bbvscraper.js
@@ -0,0 +1,60 @@
+/**
+ * Scraper für verschiedenen BBV-Online Seiten
+ */
+
+var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
+
+/**
+ * Standard URLS auf der BBV-Online Seite */
+var URLS = {
+  "schedule" : "http://www.basketball-bund.net/public/spielplan_list.jsp?print=1&viewDescKey=sport.dbb.liga.SpielplanViewPublic/index.jsp_&liga_id=",
+  "results" : "http://www.basketball-bund.net/public/liste_xl.jsp?print=1&xl=1&liga_id=",
+  "stats" : "http://www.basketball-bund.net/public/liste_xl.jsp?print=1&xl=1&liga_id="
+};
+
+/**
+ * Erstellt eine Liste mit allen Spielen eines teams
+ * @param {object} team
+ * @returns {object} schedule
+ */
+function getSchedule(team){
+  var schedule = [];
+  var url = URLS.schedule + team.liga_id;
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+      var data = $("form[name='spielplanliste']");
+      var table = data.children("table:nth-child(3)");
+      var games = table.children("tr");
+      //die erste TR muss raus
+      var game1 = table.children("tr:nth-child(2)");
+      games.splice(0,1);
+      games.each(function(i){
+        var game = $(this);
+        schedule.push({
+          "nr" : game.children("td:nth-child(1)").text().trim(),
+          "gameday" : game.children("td:nth-child(2)").text().trim(),
+          "date" : game.children("td:nth-child(3)").text().trim(),
+          "home" : game.children("td:nth-child(4)").text().trim(),
+          "guest" : game.children("td:nth-child(5)").text().trim(),
+          "location" : game.children("td:nth-child(6)").text().trim()
+        })
+      })
+    }
+    fs.writeFile(
+      "./teams/" + team.name + "/schedule.json",
+      JSON.stringify(schedule),
+      function(err){
+        if(err){
+          console.log(err);
+        }else{
+          console.log("Written file:'./teams/" + team.name + "/schedule.json'");
+        }
+      }
+    );
+  });
+}
+
+module.exports.getSchedule = getSchedule;
---
+++ teams.js
@@ -0,0 +1,9 @@
+var teams = [{
+  "name" : "damen1",
+  "liga_id" : 19042
+},{
+  "name" : "herren1",
+  "liga_id" : 19040
+}
+];
+module.exports = teams;
