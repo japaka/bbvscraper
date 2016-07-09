50e22a7da417b2ab1f57475fc62eb376453dfc15
JPK <japakaiser@gmail.com>
2016-07-09 16:50:15 +0200

    added teams

--- teams.js
+++ teams.js
@@ -4,6 +4,15 @@ var teams = [{
 },{
   "name" : "herren1",
   "liga_id" : 19040
+},{
+  "name" : "damen2",
+  "liga_id" : 19702
+},{
+  "name" : "u17w",
+  "liga_id" : 20342
+},{
+  "name" : "u15w",
+  "liga_id" : 20344
 }
 ];
 module.exports = teams;
