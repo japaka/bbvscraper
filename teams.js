bc0e9e68c36394d90d927f9fa6e37fe9e1ee2fd7
JPK <japakaiser@gmail.com>
2017-09-13 18:15:29 +0200

    Teams ergänzt

--- teams.js
+++ teams.js
@@ -13,6 +13,36 @@ var teams = [{
 },{
   "name" : "u15w",
   "liga_id" : 20344
+},{
+  "name" : "u40",
+  "liga_id" : 20572
+},{
+  "name" : "u18m",
+  "liga_id" : "20574"
+},
+{
+  "name" : "u16m",
+  "liga_id" : "20577"
+},
+{
+  "name" : "u14m",
+  "liga_id" : "20579"
+},
+{
+  "name" : "u14w",
+  "liga_id" : "20580"
+},
+{
+  "name" : "u121",
+  "liga_id" : "20581"
+},
+{
+  "name" : "u10",
+  "liga_id" : "20907"
+},
+{
+  "name" : "u122",
+  "liga_id" : "20587"
 }
 ];
 module.exports = teams;
